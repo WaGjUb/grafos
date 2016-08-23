6559c846eddfb6588c427239e0fa18a320763df2
wagjub <danielcostavalerio@hotmail.com>
2016-08-23 17:05:57 -0300

    enviado v0.1

--- representacaoGrafos.py
+++ representacaoGrafos.py
@@ -9,19 +9,19 @@ class aresta(object):
         self.destino = destino
 
 class vertice(object):
-    def __init__(self):
-        self.valor = None
+    def __init__(self, valor):
+        self.valor = valor
 
 class grafo(object):
     def __init__(self):
         self.Larestas = []
         self.Lvertices = [[]]
 
-    def addAresta(self, aresta):
+    def adicionarAresta(self, aresta):
         self.Larestas.append(aresta)
 
-    def addVertice(self, vertice): ##
-        self.Lvertices.insert(vertice)
+    def adicionarVertice(self, vertice): ##
+        self.Lvertices.append(vertice)
 
     def removerVertice(self, vertice): ##
         try:
@@ -48,15 +48,61 @@ class grafo(object):
         except ValueError:
             return None
         
-if __name__ == "__main__":
-    entrada = None
+def caso_1(g):
+    valor = input('Digite o valor do vertice: ')
+    v = vertice(valor)
+    g.adicionarVertice(v)
+    
+def caso_2(g):
+    valor = input('Digite o valor do vertice: ')
+    v = vertice(valor)
+    g.removerVertice(v)
 
-    while entrada != 0:
-        menu()
-        entrada = input(": ")
-        if (entrada > 7 || entrada < 0):
-            print('\n'*100)
-            print("Digite uma opção válida!")
+def caso_3(g):
+    valor = input('Digite a origem e destino separado por vírgula Ex. o,d: ').split(',')
+    if len(valor) != 2:
+        a = aresta(valor[0], valor[1])
+        g.adicionarAresta(a)
+
+def caso_4(g):
+    valor = input('Digite a origem e destino separado por vírgula Ex. o,d: ').split(',')
+    if len(valor) != 2:
+        a = aresta(valor[0], valor[1])
+        g.removerAresta(a)
+
+def caso_5(g):
+    valor = input('Digite o valor do vertice: ')
+    v = vertice(valor)
+    g.buscarVertice(v)
+
+def caso_6(g):
+    valor = input('Digite a origem e destino separado por vírgula Ex. o,d: ').split(',')
+    if len(valor) != 2:
+        a = aresta(valor[0], valor[1])
+        g.buscarAresta(a)
+
+def printMenu():
+    print("1 - Adicionar vertice",
+          "\n2 - Remover vertice",
+          "\n3 - Adicionar aresta", 
+          "\n4 - Remover aresta",
+          "\n5 - Buscar vertice", 
+          "\n6 - Buscar aresta"
+          "\n7 - Sair")
+
+if __name__ == "__main__":
+    entrada = 10
+    g = grafo()
+    mendict = {1: caso_1, 2: caso_2, 3: caso_3, 4: caso_4, 5: caso_5, 6: caso_6}
+    while True:
+        printMenu()
+        entrada = int(input("Opção: "))
+        if entrada == 7:
+            break
         else:
+#            try:
+            mendict[entrada](g)
+#            except:
+#                print("Digite uma opção valida!!")
 
 
